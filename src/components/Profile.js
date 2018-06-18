be987686d252428e6029f3639cf604eaca4a074e
Brian Andrews <sbardian@gmail.com>
2018-06-17 21:21:14 -0600

    Add logic for selecting profile image for upload

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,10 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import {
-  Icon,
-  Image,
-  ImageBackground,
-  Platform,
-  StyleSheet,
-  View,
-} from 'react-native';
-import { H1, Text } from 'native-base';
+import { Alert, Image, ImageBackground, StyleSheet, View } from 'react-native';
+import { Button, Text } from 'native-base';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
-import { styles as s } from 'react-native-style-tachyons';
+import { ImagePicker, Permissions } from 'expo';
 
 const GET_MY_INFO = gql`
   {
@@ -33,9 +26,10 @@ const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#AE24FD',
+    // backgroundColor: '#AE24FD',
+    backgroundColor: 'white',
   },
-  loginForm: {
+  profileImage: {
     backgroundColor: 'transparent',
     alignItems: 'center',
   },
@@ -55,7 +49,53 @@ const styles = StyleSheet.create({
 });
 
 export class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+      profileStatus: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (status !== 'granted') {
+      this.setState({
+        profileStatus: true,
+      });
+      Alert.alert(
+        'Profile Image',
+        `You have not granted permission to access your images, you will not be able to update your profile image.`,
+        [
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+          },
+          {
+            text: 'OK',
+            onPress: () => console.log('Ok Pressed'),
+          },
+        ],
+        { cancelable: true },
+      );
+    } else {
+      this.setState({
+        profileStatus: false,
+      });
+    }
+  }
+
+  pickImage = async () => {
+    const pickerResult = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+    if (!pickerResult.cancelled) this.setState({ image: pickerResult.uri });
+  };
+
   render() {
+    let { image } = this.state;
     return (
       <Query query={GET_MY_INFO}>
         {({ loading, error, data: { getMyInfo } = [] }) => {
@@ -66,19 +106,43 @@ export class Profile extends Component {
           return (
             <View style={styles.container}>
               <View style={styles.headerContainer}>
-                <View style={styles.backgroundImage}>
-                  <View style={styles.loginForm}>
+                <ImageBackground
+                  source={require('../images/background.png')}
+                  style={styles.backgroundImage}
+                >
+                  <View style={styles.profileImage}>
                     <Image
                       style={styles.userImage}
                       source={require('../images/defaultProfile.jpg')}
                     />
                     <Text style={styles.text}>{username}</Text>
                   </View>
-                </View>
+                </ImageBackground>
               </View>
               <View>
                 <Text>Email: {email}</Text>
                 <Text>Profile Image: </Text>
+                <Button
+                  bordered
+                  disabled={this.state.profileStatus}
+                  onPress={this.pickImage}
+                >
+                  <Text>Select</Text>
+                </Button>
+                {this.state.image && (
+                  <View
+                    style={{
+                      borderTopRightRadius: 3,
+                      borderTopLeftRadius: 3,
+                      overflow: 'hidden',
+                    }}
+                  >
+                    <Image
+                      source={{ uri: image }}
+                      style={{ width: 250, height: 250 }}
+                    />
+                  </View>
+                )}
               </View>
             </View>
           );
