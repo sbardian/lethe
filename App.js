cd86e5e6e9385761f4f75509a3056f48bd08c9e6
Brian Andrews <sbardian@gmail.com>
2018-06-28 14:59:40 -0600

    Update ws link to use wss protocol

--- App.js
+++ App.js
@@ -76,7 +76,7 @@ export default class App extends Component {
                 // uri: 'http://10.0.3.2:9999/graphql',
                 // ios
                 // uri: 'http://localhost:9999/graphql',
-                uri: 'https://letheapi-mmfmwkptgd.now.sh/graphql',
+                uri: 'https://letheapi-vbkipxhmsh.now.sh/graphql',
               });
 
               const wsLink = new WebSocketLink({
@@ -85,7 +85,7 @@ export default class App extends Component {
                 // ios
                 // uri: 'ws://localhost:9999/graphql',
                 // now
-                uri: `https://letheapi-mmfmwkptgd.now.sh/subscriptions`,
+                uri: `wss://letheapi-vbkipxhmsh.now.sh/subscriptions`,
                 options: {
                   reconnect: true,
                   connectionParams: {
