ab75cf3403dd0bbe446c8bd0577531df2b39ad62
Brian Andrews <sbardian@gmail.com>
2018-10-01 21:54:03 -0600

    New API urls

--- App.js
+++ App.js
@@ -76,7 +76,7 @@ export default class App extends Component {
                 // uri: 'http://10.0.3.2:9999/graphql',
                 // ios
                 // uri: 'http://localhost:9999/graphql',
-                uri: 'https://letheapi-yjgjiqkuec.now.sh/graphql',
+                uri: 'https://letheapi-prjwpfgctu.now.sh/graphql',
               });
 
               const wsLink = new WebSocketLink({
@@ -85,7 +85,7 @@ export default class App extends Component {
                 // ios
                 // uri: 'wss://localhost:9999/subscriptions',
                 // now
-                uri: `wss://letheapi-yjgjiqkuec.now.sh/subscriptions`,
+                uri: `wss://letheapi-prjwpfgctu.now.sh/subscriptions`,
                 options: {
                   reconnect: true,
                   connectionParams: {
