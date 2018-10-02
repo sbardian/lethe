78710e75347258dfc3b4c73e31a4fbec8df17ae9
Brian Andrews <sbardian@gmail.com>
2018-10-02 11:50:00 -0600

    Add propTypes to Screen component

--- src/screens/Screen.js
+++ src/screens/Screen.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Container, Content } from 'native-base';
 
-export const Screen = ({ screenStyles, children, fab }) => (
-  <Container style={screenStyles}>
+export const Screen = ({ children, fab }) => (
+  <Container>
     <Content>{children}</Content>
     {fab}
   </Container>
 );
+
+Screen.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.node),
+  fab: PropTypes.func,
+};
+
+Screen.defaultProps = {
+  children: null,
+  fab: () => {},
+};
