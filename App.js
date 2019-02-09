9de3a29d333e0b35a4407cb7dd0afb6cfe7b4124
Brian Andrews <sbardian@gmail.com>
2019-02-08 22:06:37 -0700

    Update font async load

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import NativeTachyons from 'react-native-style-tachyons';
 import { AsyncStorage, StyleSheet } from 'react-native';
 import { Root } from 'native-base';
-import Expo from 'expo';
+import { AppLoading, Font } from 'expo';
 import { ApolloClient } from 'apollo-client';
 import { from, split } from 'apollo-link';
 import { getMainDefinition } from 'apollo-utilities';
@@ -38,25 +38,48 @@ export default class App extends Component {
     };
   }
 
-  async componentWillMount() {
-    await Expo.Asset.fromModule(
-      require('./src/images/brian.png'),
-      require('./src/images/background.png'),
-      require('./src/images/defaultProfile.jpg'),
-    ).downloadAsync();
-    await Expo.Font.loadAsync({
+  // async componentWillMount() {
+  //   await Expo.Asset.fromModule(
+  //     require('./src/images/brian.png'),
+  //     require('./src/images/background.png'),
+  //     require('./src/images/defaultProfile.jpg'),
+  //   ).downloadAsync();
+  //   await Expo.Font.loadAsync({
+  //     Roboto: require('native-base/Fonts/Roboto.ttf'),
+  //     Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+  //     Ionicons: require('native-base/Fonts/Ionicons.ttf'),
+  //   });
+
+  //   this.setState({ isReady: true });
+  // }
+
+  loadFontsAsync = () =>
+    Font.loadAsync({
+      // Roboto fonts are required for native-base and pre-loading them here
+      // avoids a potential race condition of attempting to render some NB
+      // component before the fonts have been loaded.
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      Ionicons: require('native-base/Fonts/Ionicons.ttf'),
     });
 
-    this.setState({ isReady: true });
-  }
+  handleLoadingFailure = err => {
+    // TODO: report to error service?
+    // eslint-disable-next-line no-console
+    console.error(err);
+  };
+
+  handleLoadingSuccess = () => this.setState({ isReady: true });
 
   render() {
     const { isReady } = this.state;
     if (!isReady) {
-      return <Expo.AppLoading />;
+      return (
+        <AppLoading
+          startAsync={this.loadFontsAsync}
+          onError={this.handleLoadingFailure}
+          onFinish={this.handleLoadingSuccess}
+        />
+      );
     }
     return (
       <Root>
@@ -77,18 +100,18 @@ export default class App extends Component {
                 // android
                 // uri: 'http://10.0.3.2:9999/graphql',
                 // ios
-                // uri: 'http://localhost:9999/graphql',
+                uri: 'http://localhost:9999/graphql',
                 // now
-                uri: 'https://letheapi-drnljuhskx.now.sh/graphql',
+                // uri: 'https://letheapi-drnljuhskx.now.sh/graphql',
               });
 
               const wsLink = new WebSocketLink({
                 // android
                 // uri: 'wss://10.0.3.2:9999/subscriptions',
                 // ios
-                // uri: 'wss://localhost:9999/subscriptions',
+                uri: 'wss://localhost:9999/subscriptions',
                 // now
-                uri: `wss://letheapi-drnljuhskx.now.sh/subscriptions`,
+                // uri: `wss://letheapi-drnljuhskx.now.sh/subscriptions`,
                 options: {
                   reconnect: true,
                   connectionParams: {
