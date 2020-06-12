911147f3d6d45f7edd910215ababcc9dee207ee7
Brian Andrews <sbardian@gmail.com>
2020-06-11 20:50:31 -0600

    test: simplify test

--- src/components/FabBase.test.js
+++ src/components/FabBase.test.js
@@ -2,17 +2,13 @@ import React from 'react';
 import { render } from '../test-utils/custom-renderer';
 import { FabBase } from './FabBase';
 
-const navigation = {
-  navigate: jest.fn(),
-};
-
 describe('FabBase', () => {
   it('Render a base fab using add icon', async () => {
     const { getByTestId } = render(
       <FabBase
         backgroundColor="#8CD19D"
         position="bottomRight"
-        onPress={() => navigation.navigate('AddList')}
+        onPress={() => {}}
         iconName="add"
         iconType="Ionicons"
       />,
