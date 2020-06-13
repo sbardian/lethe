174da6747e642d2f3882cb33207127c149ea1a19
Brian Andrews <sbardian@gmail.com>
2020-06-12 21:32:38 -0600

    chore: update deps and tweak jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,5 @@
-import '@testing-library/jest-dom/extend-expect';
+// import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-native/extend-expect';
 import { cleanup } from '@testing-library/react-native';
 import { setupServer } from 'msw/node';
 import { handlers } from './src/test-utils/mock-server';
